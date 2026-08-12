1249c31240183d962546cb95df77f57f0455c3a0
pghyl <pghyl@local>
2026-08-12 21:33:18 +0800

    chore: bump Qin PWA cache to 1.0.6

--- js/pwa.js
+++ js/pwa.js
@@ -1,7 +1,7 @@
 (function () {
   "use strict";
 
-  var APP_VERSION = "1.0.5";
+  var APP_VERSION = "1.0.6";
   var registration = null;
   var waitingWorker = null;
   var deferredInstallPrompt = null;
--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const CACHE_PREFIX = "qinshi-site-";
-const CACHE_NAME = CACHE_PREFIX + "1.0.5";
+const CACHE_NAME = CACHE_PREFIX + "1.0.6";
 const PRECACHE_URLS = [
   "./",
   "./index.html",

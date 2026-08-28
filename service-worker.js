e05f079059288c925624c54746e195125ff50a06
pghyl <pghyl@local>
2026-08-28 16:38:15 +0800

    chore: release pwa 1.0.17

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const CACHE_PREFIX = "qinshi-site-";
-const CACHE_NAME = CACHE_PREFIX + "1.0.16";
+const CACHE_NAME = CACHE_PREFIX + "1.0.17";
 const PRECACHE_URLS = [
   "./",
   "./index.html",
